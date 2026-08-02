0749508ccc9e324ac491fca784c2fcfe72b979c7
phamthanhlong1005 <166156509+phamthanhlong100504-xpnd@users.noreply.github.com>
2026-08-02 23:55:31 +0700

    update

--- src/main/java/com/dts/media/MediaApplication.java
+++ src/main/java/com/dts/media/MediaApplication.java
@@ -3,12 +3,12 @@ package com.dts.media;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-// import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-// @EnableDiscoveryClient
-// @EnableFeignClients
+@EnableDiscoveryClient
+@EnableFeignClients
 public class MediaApplication {
 
 	public static void main(String[] args) {
