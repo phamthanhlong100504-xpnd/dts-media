150a0e9763c843e3e5b05b37d7fada47d942f6c3
phamthanhlong1005 <166156509+phamthanhlong100504-xpnd@users.noreply.github.com>
2026-08-03 12:40:12 +0700

    Fix Lazy Initialization bug for Kafka Consumer

--- src/main/java/com/dts/media/infrastructure/worker/UploadVerificationConsumer.java
+++ src/main/java/com/dts/media/infrastructure/worker/UploadVerificationConsumer.java
@@ -5,11 +5,13 @@ import com.dts.media.application.service.UploadService;
 import com.dts.media.infrastructure.configuration.KafkaConfiguration;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
+@Lazy(false)
 @RequiredArgsConstructor
 public class UploadVerificationConsumer {
 
